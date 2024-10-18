9fdb5dbb47c37bede072a32339a5654cfcb1868c
gowreesh <gowreesh369@gmail.com>
2024-10-18 10:00:00 +0530

    cicd setup

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'jest'
+import nextJest from 'next/jest'
+
+const createJestConfig = nextJest({
+    dir: './',
+})
+
+const config: Config = {
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    testEnvironment: 'jest-environment-jsdom',
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/$1',
+    },
+    testMatch: ['**/*.test.ts', '**/*.test.tsx'],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+}
+
+export default createJestConfig(config)
